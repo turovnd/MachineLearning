0773aacbca1ab6af8bf81e35b6b2c036cdd0380b
Fedorov Ilya <fedyi95@mail.ru>
2017-11-07 20:00:41 +0300

    build2DInfo done

--- 2. Linear regression/Visualization.py
+++ 2. Linear regression/Visualization.py
@@ -37,16 +37,14 @@ class Visualization(object):
         return 0
 
     @staticmethod
-    def buildNewDatasetPlot(data, newZ):
+    def buildNewDatasetPlot(w, data, newZ, w0, w1, MSE, newPrice):
         x, y, z = DatasetProcessing.getSeparetedData(data)
         fig = plt.figure()
         axes = Axes3D(fig)
         axes.scatter(x, y, z, color="#00CC00")
         axes.scatter(x, y, newZ, color="#FF0000")
-        X_NP = np.vstack((x, y, z))
-
-        print(X_NP)
-        # axes.scatter(x[:, 1], y[:, 1], newZ, color='0000FF')
+        # axes.scatter(x, y, w0, color="#FFCC00")
+        # axes.plot_wireframe(x, newZ, newPrice, color="#FFCC00")
         # заголовок
         plt.title("Отображение обновленного? датасета")
         # подпись осей
@@ -56,15 +54,41 @@ class Visualization(object):
         plt.show()
         return 0
 
+    """Метод отображения нескольких 2D графиков за один вызов.
+        1) Изменение среднеквадратичного отклонения с каждой итерацией.
+        2) Визуализация данных и вычисленных цен.
+        
+    Args: 
+        price: лист, содержащий цену из датасета.
+        newPrice: лист, содержащий пересчитанную цену.
+        MSE: лист, содержащий среднеквадртичные отклоения.
+
+    Returns:
+        0: удачное исполнение.
+    """
     @staticmethod
-    def buildErrorDatasetPlot(j_hist):
-
-        x = np.arange(1, len(j_hist) + 1)
-        y = j_hist
-        plt.plot(x, j_hist)
+    def build2DInfo(price, newPrice, MSE):
+        plt.subplot(211)
+        plt.plot(MSE, '-', color="#560EAD", linewidth=4)
+        plt.title("Изменение среднеквадратичного отклонения с каждой итерацией")
+        plt.xlabel("$iteration$")
+        plt.ylabel("$J(w) - MSE$")
+        plt.grid(True)
+        plt.subplot(212)
+        colors = ["#00B945", "#FF2C00"]
+        linewidths = [4, 4]
+        labels = ["predicted price", "given price"]
+        plt.plot([newPrice[0][i] for i in range(len(newPrice[0]))], color=colors[0],
+                 linewidth=linewidths[0], label=labels[0])
+        plt.plot([price[i] for i in range(len(price))], color=colors[1],
+                 linewidth=linewidths[1], label=labels[1])
         # заголовок
-        plt.title("Отображение обновленного? датасета")
+        plt.title("Визуализация данных и вычисленных цен")
         # подпись осей
-
+        plt.xlabel("$iteration$")
+        plt.ylabel("$price$")
+        # легенда графика
+        plt.legend(loc=1, fontsize="small")
+        plt.grid(True)
         plt.show()
         return 0
