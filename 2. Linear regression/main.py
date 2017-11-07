c787eaa9470204b3f734c25fd57c39ca66dd8146
Fedorov Ilya <fedyi95@mail.ru>
2017-11-07 23:39:01 +0300

    main update

--- 2. Linear regression/main.py
+++ 2. Linear regression/main.py
@@ -14,19 +14,32 @@ if __name__ == '__main__':
     print("rooms =", rooms)
     print("price =", price)
 
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 10, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 1, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.1, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.01, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.05, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.001, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.005, 50000, 0.00000001)
-    (lastIteration, MSE, w, newPrice, w1, w2) = GradientDescent.calculateGradientDescent(data, 0.0001, 2400, 0.0000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.0001, 24000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.0005, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.00001, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.00005, 50000, 0.00000001)
-    # (j_hist, w, newPrice, w0, w1) = GradientDescent.calculateGradientDescent(data, 0.000001, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 10, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 1, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.1, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.01, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.05, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.001, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.005, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.0001, 2400, 0.0000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.0001, 24000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.0005, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.00001, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.00005, 50000, 0.00000001)
+    (lastIteration, MSE, w, newPrice, w1, w2) = \
+        GradientDescent.calculateGradientDescent(data, 0.000001, 50000, 0.00000001)
 
     # print("j_hist =", j_hist)
     # newPrice_NP = np.asarray(newPrice)
