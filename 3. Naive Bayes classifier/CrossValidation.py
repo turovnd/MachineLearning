06fffd13a55373255d85624d789607796fd27d5f
Nikolay <turov96@yandex.ru>
2017-11-28 14:50:06 +0300

    lab 3 fix - CrossValidation

--- 3. Naive Bayes classifier/CrossValidation.py
+++ 3. Naive Bayes classifier/CrossValidation.py
@@ -1,3 +1,6 @@
+import random
+
+
 def get_metrics(bayes, data, debug):
     # true positive is a spam which is predicted well
     # true negative is a not spam which is predicted well
@@ -40,40 +43,27 @@ class Validator(object):
         pass
 
     @staticmethod
-    def validate(bayes, data, debug=False, cross_valid=False):
+    def validate(bayes, data, debug=False):
 
-        if not cross_valid:
-            train_set = sum(data[:-5], [])
-            test_set = data[-5]
+        f_measures = []
+        matrix_full = [[0, 0], [0, 0]]
+        for i in range(len(data)):
+            first_part = data[:(len(data) - 1) - i]
+            second_part = data[(len(data) - 1) - i + 1:]
+
+            train_set = first_part + second_part
+            train_set = sum(train_set, [])
+            test_set = data[(len(data) - 1) - i]
+
+            bayes.reset()
             bayes.train(train_set)
-            return get_metrics(bayes, test_set, debug)
 
-        else:
-            f_measures = []
-            matrix_full = [[0, 0], [0, 0]]
+            f_measure, matrix = get_metrics(bayes, test_set, debug)
 
-            for i in range(len(data)):
-                first_part = data[:(len(data) - 1) - i]
-                second_part = data[(len(data) - 1) - i + 1:]
+            matrix_full[0][0] += matrix[0][0]
+            matrix_full[0][1] += matrix[0][1]
+            matrix_full[1][0] += matrix[1][0]
+            matrix_full[1][1] += matrix[1][1]
+            f_measures.append(f_measure)
 
-                train_set = first_part + second_part
-                train_set = sum(train_set, [])
-                test_set = data[(len(data) - 1) - i]
-
-                bayes.reset()
-                bayes.train(train_set)
-
-                f_measure, matrix = get_metrics(bayes, test_set, debug)
-
-                matrix_full[0][0] += matrix[0][0]
-                matrix_full[0][1] += matrix[0][1]
-                matrix_full[1][0] += matrix[1][0]
-                matrix_full[1][1] += matrix[1][1]
-                f_measures.append(f_measure)
-
-            matrix_full[0][0] = round(matrix_full[0][0] / len(f_measures))
-            matrix_full[0][1] = round(matrix_full[0][1] / len(f_measures))
-            matrix_full[1][0] = round(matrix_full[1][0] / len(f_measures))
-            matrix_full[1][1] = round(matrix_full[1][1] / len(f_measures))
-
-            return sum(f_measures) / len(f_measures), matrix_full
+        return sum(f_measures) / len(f_measures), matrix_full
